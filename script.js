a8affa5820ccb6b206359d06f077d2d9a127dffc
bhargabLinx <bhargabbhuyan001@gmail.com>
2024-05-31 17:11:45 +0530

    Displays the result of the calculation as well as the number while entering them

--- script.js
+++ script.js
@@ -3,29 +3,58 @@ let secondOperand = null;
 let operator = null;
 
 const numbers = document.querySelectorAll(".num");
+const operation = document.querySelectorAll(".cal");
+const ac = document.querySelector(".ac");
+const equal = document.querySelector(".equal");
+const screen = document.querySelector(".screen");
 
 numbers.forEach((btn) => {
     btn.addEventListener('click', () => {
         if (firstOperand == null) {
             firstOperand = parseInt(btn.textContent);
+            screen.textContent = firstOperand;
         } else {
             secondOperand = parseInt(btn.textContent);
+            screen.textContent = secondOperand;
         }
     });
+});
+
+operation.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        if (operator == null) {
+            operator = btn.textContent;
+        }
+    });
+});
+
+ac.addEventListener('click', () => {
+    firstOperand = null;
+    secondOperand = null;
+    operator = null;
+    screen.textContent = "0";
+})
+
+equal.addEventListener('click', () => {
+    calculate(firstOperand, secondOperand, operator);
 });
 
 function calculate(firstOperand, secondOperand, operator) {
     switch(operator) {
         case '+':
+            screen.textContent = `${firstOperand + secondOperand}`;
             console.log(firstOperand + secondOperand);
             break;
         case '-':
+            screen.textContent = `${firstOperand - secondOperand}`;
             console.log(firstOperand - secondOperand);
             break;
         case '*':
+            screen.textContent = `${firstOperand * secondOperand}`;
             console.log(firstOperand * secondOperand);
             break;
         case '/':
+            screen.textContent = `${firstOperand / secondOperand}`;
             console.log(firstOperand / secondOperand);
             break;
     }
