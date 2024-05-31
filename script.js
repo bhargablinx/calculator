f6d4d621712755501d93e2b8ed58a8b2c153e645
bhargabLinx <bhargabbhuyan001@gmail.com>
2024-05-31 17:42:45 +0530

    Added functionality for multiple numbers

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 let firstOperand = null;
 let secondOperand = null;
 let operator = null;
+let result = null;
+const MAX_DIGITS = 10;
 
 const numbers = document.querySelectorAll(".num");
 const operation = document.querySelectorAll(".cal");
 const ac = document.querySelector(".ac");
 const equal = document.querySelector(".equal");
-const screen = document.querySelector(".screen");
+const  screen = document.querySelector(".screen");
+
+function addToScreen(currentDisplay, digit) {
+    if (currentDisplay.length < MAX_DIGITS) {
+        screen.textContent = `${currentDisplay}${digit}`;
+    } else
+        screen.textContent = `${currentDisplay}`;
+}
+
+let currentDisplay = () => {
+    return screen.textContent;
+};
 
 numbers.forEach((btn) => {
     btn.addEventListener('click', () => {
         if (firstOperand == null) {
             firstOperand = parseInt(btn.textContent);
             screen.textContent = firstOperand;
+        } else if (operator == null) {
+            secondOperand = parseInt(btn.textContent);
+            addToScreen(currentDisplay(), secondOperand);
         } else {
+            firstOperand = parseInt(screen.textContent);
             secondOperand = parseInt(btn.textContent);
             screen.textContent = secondOperand;
         }
@@ -42,23 +59,20 @@ equal.addEventListener('click', () => {
 function calculate(firstOperand, secondOperand, operator) {
     switch(operator) {
         case '+':
-            screen.textContent = `${firstOperand + secondOperand}`;
-            console.log(firstOperand + secondOperand);
+            result = firstOperand + secondOperand;
+            screen.textContent = result;
             break;
         case '-':
-            screen.textContent = `${firstOperand - secondOperand}`;
-            console.log(firstOperand - secondOperand);
+            result = firstOperand - secondOperand;
+            screen.textContent = result;
             break;
         case '*':
-            screen.textContent = `${firstOperand * secondOperand}`;
-            console.log(firstOperand * secondOperand);
+            result = firstOperand * secondOperand;
+            screen.textContent = result;
             break;
         case '/':
-            screen.textContent = `${firstOperand / secondOperand}`;
-            console.log(firstOperand / secondOperand);
+            result = firstOperand / secondOperand;
+            screen.textContent = result;
             break;
     }
 }
-
-console.log(firstOperand);
-console.log(secondOperand);
